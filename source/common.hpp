51dfdbaed2f4e364eb2f1eb6857e69601b4c198a
Sebastian Green-Husted <ricecake@tfm.nu>
2014-11-10 18:30:23 -0500

    flesh out config class a bit

--- source/common.hpp
+++ source/common.hpp
@@ -1,5 +1,8 @@
-#include <stddef.h>  // defines NULL
+#ifndef __COMMON_HPP_
+#define __COMMON_HPP_
+
 #include <mutex>
+#include <stddef.h>  // defines NULL
 
 template <class T> class Singleton {
 	public:
@@ -17,48 +20,11 @@ template <class T> class Singleton {
 		}
 	protected:
 	private:
-		Singleton(Singleton const&);
+		Singleton(Singleton const&) {}
 		Singleton& operator=(Singleton const&);
 		static T* m_pInstance;
 };
 
 template <class T> T* Singleton<T>::m_pInstance=NULL;
 
-template<class T, class M> class locking_ptr {
-	public:
-		locking_ptr(T* ptr): m_ptr(ptr) {
-			m_mutex = new M;
-			m_mutex->lock();
-		}
-		locking_ptr(T* ptr, M* lock): m_ptr(ptr), m_mutex(lock) {
-			m_mutex->lock();
-		}
-		~locking_ptr() {
-			if (m_mutex)
-				m_mutex->unlock();
-		}
-		locking_ptr(locking_ptr<T, M>&& ptr): m_ptr(ptr.m_ptr), m_mutex(ptr.m_mutex) {
-			ptr.m_ptr = nullptr;
-			ptr.m_mutex = nullptr;
-		}
-
-		T* operator ->() {
-			return m_ptr;
-		}
-		T const* operator ->() const {
-			return m_ptr;
-		}
-		T& operator * () const throw () {
-			return *m_ptr;
-		}
-
-	private:
-		// disallow copy/assignment
-		locking_ptr(locking_ptr<T, M> const& ptr) {}
-		locking_ptr& operator = (locking_ptr<T, M> const& ptr) {
-			return *this;
-		}
-		T* m_ptr;
-		M* m_mutex; // whatever implementation you use
-};
-
+#endif
--- source/config.hpp
+++ source/config.hpp
@@ -1,5 +1,21 @@
 #include<common.hpp>
 #include <libconfig.h++>
-
+#include<iostream>
 typedef Singleton<libconfig::Config> Config;
 typedef Singleton<libconfig::Config>* ConfigPtr;
+typedef Singleton<std::mutex> ConfigMutex;
+
+class asConfig {
+        public:
+                asConfig() {
+                        lock->lock();
+                        confObj = new Config;
+                }
+                ~asConfig() {
+                        delete confObj;
+                        lock->unlock();
+                }
+        private:
+                ConfigMutex lock;
+                ConfigPtr confObj;
+};
