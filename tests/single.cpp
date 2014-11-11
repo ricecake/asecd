eb22d4bc6bb309803b8295a265401f6c3dfc1b44
Sebastian Green-Husted <ricecake@tfm.nu>
2014-11-11 18:42:12 -0500

    demonstrate that the locks work as expected

--- tests/single.cpp
+++ tests/single.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<common.hpp>
+#include<thread>
 #include <mutex>
 #include<string>
 #include<config.hpp>
@@ -7,9 +8,20 @@
 typedef Singleton<std::string> Config2;
 typedef Singleton<std::mutex> ConfigMutex2;
 int main() {
-	asConfig asc;
-	asc.conf()->readFile("example.cfg");
-	std::string setting = asc.conf()->lookup("name");
-	std::cout << setting  << std::endl;
+
+	std::thread* array[5];
+	for (int i=0; i<5;i++) {
+		array[i] = new std::thread([i]{
+			std::cout<< i << std::endl;
+			asConfig* asc = new asConfig;
+			asc->conf()->readFile("example.cfg");
+			std::string setting = asc->conf()->lookup("name");
+			std::cout << setting  << std::endl;
+			delete asc;
+		});
+	}
+	for (auto i : array) {
+		i->join();
+	}
 	//ConfigPtr conf = new Config;
 }
